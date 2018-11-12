bb633d3ff83ebfc2c4ed50fb727fc71db497f240
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-11-12 20:42:27 +0100

    Try to spawn multiple enemy

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var config = {
     preload: preload,
     create: create,
     update: update,
+    createEnemyAttack: createEnemyAttack,
   }
 };
 
@@ -39,49 +40,48 @@ let starship;
 var bullets;
 var flipFlop;
 var enemyAttack;
+var allowSpawn = true;
+
 
 function create() {
-
   sprite = this.add.sprite(240, 280, 'background');
 
+
   starship = this.physics.add.sprite(10, 20, 'starship');
-  enemyAttack = this.physics.add.image(700, 200, 'enemyAttack');
 
   starship.setCollideWorldBounds(true);
   starship.scaleX = 0.2;
   starship.scaleY = 0.2;
+  starship.physicsBodyType = Phaser.Physics.ARCADE;
+
 
 
   starship.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
   starship.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
   starship.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACEBAR);
-  key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
 
   cursors = this.input.keyboard.createCursorKeys();
 
 
-  enemyAttack.setVelocity(200, 0);
-  enemyAttack.setBounce(1, 1);
-  enemyAttack.scaleX = 0.6;
-  enemyAttack.scaleY = 0.6;
-  enemyAttack.setCollideWorldBounds(true);
-  enemyAttack.enableBody = true;
-  enemyAttack.angle = 270;
-  enemyAttack.physicsBodyType = Phaser.Physics.ARCADE;
-  enemyAttack.body.immovable = true;
-  
+  this.enemys = this.add.group();
+
+  // game.time.events.loop(100, createEnemyAttack, this);
+
+
   bullets = this.add.group();
   bullets.enableBody = true;
   bullets.physicsBodyType = Phaser.Physics.ARCADE;
-  bullets.createMultiple(50, 'fire');
+  // bullets.createMultiple(50, 'fire');
+  this.enemyAttack = this.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
 
   this.physics.add.collider(bullets, enemyAttack);
-  
-  
+
+
 }
 
 function update() {
-  
+
+
   if (cursors.up.isDown) // if the up arrow key is down
   {
     starship.y -= 5;
@@ -97,8 +97,8 @@ function update() {
       bullets.setVelocity(800, 0);
       flipFlop = true;
       this.physics.add.overlap(bullets, enemyAttack, destroyEnemy, null, this);
-      
-      
+
+
     }
   }
   if (cursors.space.isUp) {
@@ -106,6 +106,33 @@ function update() {
   }
 }
 
-function destroyEnemy(bullets, enemyattack){
+function destroyEnemy() {
   enemyAttack.disableBody(true, true);
+}
+
+function createEnemyAttack() {
+  enemyAttack = this.physics.add.image(700, 200, 'enemyAttack');
+
+  // enemyAttack = game.physics.add.group({
+  //   key: 'enemyAttack',
+  //   repeat: 3,
+  //   setXY: {
+  //     x: 1300,
+  //     y: 900,
+  //     // stepX: 70
+  //   }
+  // });
+  // randomPos = Math.round(Math.random() * 900);
+  // console.log(enemyAttack.x);
+  enemyAttack.setVelocity(-200, 0);
+  // enemyAttack.setBounce(1, 1);
+  enemyAttack.scaleX = 0.6;
+  enemyAttack.scaleY = 0.6;
+  enemyAttack.setCollideWorldBounds(true);
+  // enemyAttack.enableBody = true;
+  enemyAttack.angle = 270;
+  // enemyAttack.physicsBodyType = Phaser.Physics.ARCADE;
+  // enemyAttack.body.immovable = true;
+
+
 }
