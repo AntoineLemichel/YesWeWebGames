4099451002b079fda280afc2112608f1c13ee351
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-11-12 16:01:48 +0100

    Adding shoot for player and collider with bullets

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 1200,
   height: 600,
   physics: {
     default: 'arcade',
@@ -21,7 +21,7 @@ function preload() {
   // this.load.setBaseURL('http://localhost/spaceInvaders/');
 
   this.load.spritesheet('background', '/backgroundSpriteSheet.png', {
-    frameWidth: 1200,
+    frameWidth: 2000,
     frameHeight: 1200
   }, 18);
   this.load.image('starship', './startship.png');
@@ -37,17 +37,20 @@ function preload() {
 var key;
 let starship;
 var bullets;
+var flipFlop;
+var enemyAttack;
 
 function create() {
 
   sprite = this.add.sprite(240, 280, 'background');
 
   starship = this.physics.add.sprite(10, 20, 'starship');
-  var enemyAttack = this.physics.add.image(700, 200, 'enemyAttack');
+  enemyAttack = this.physics.add.image(700, 200, 'enemyAttack');
 
   starship.setCollideWorldBounds(true);
   starship.scaleX = 0.2;
   starship.scaleY = 0.2;
+
 
   starship.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
   starship.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
@@ -59,19 +62,26 @@ function create() {
 
   enemyAttack.setVelocity(200, 0);
   enemyAttack.setBounce(1, 1);
+  enemyAttack.scaleX = 0.6;
+  enemyAttack.scaleY = 0.6;
   enemyAttack.setCollideWorldBounds(true);
-
+  enemyAttack.enableBody = true;
+  enemyAttack.angle = 270;
+  enemyAttack.physicsBodyType = Phaser.Physics.ARCADE;
+  enemyAttack.body.immovable = true;
+  
   bullets = this.add.group();
   bullets.enableBody = true;
   bullets.physicsBodyType = Phaser.Physics.ARCADE;
-
   bullets.createMultiple(50, 'fire');
 
-
+  this.physics.add.collider(bullets, enemyAttack);
+  
+  
 }
 
 function update() {
-
+  
   if (cursors.up.isDown) // if the up arrow key is down
   {
     starship.y -= 5;
@@ -80,10 +90,22 @@ function update() {
     starship.y += 5;
   }
   if (cursors.space.isDown) {
-    bullets = this.physics.add.sprite(starship.x, starship.y, 'fire');
-
-    bullets.scaleX = 0.2;
-    bullets.scaleY = 0.2;
-    bullets.setVelocity(800, 0);
+    if (!flipFlop) {
+      bullets = this.physics.add.sprite(starship.x, starship.y, 'fire');
+      bullets.scaleX = 0.2;
+      bullets.scaleY = 0.2;
+      bullets.setVelocity(800, 0);
+      flipFlop = true;
+      this.physics.add.overlap(bullets, enemyAttack, destroyEnemy, null, this);
+      
+      
+    }
   }
+  if (cursors.space.isUp) {
+    flipFlop = false;
+  }
+}
+
+function destroyEnemy(bullets, enemyattack){
+  enemyAttack.disableBody(true, true);
 }
